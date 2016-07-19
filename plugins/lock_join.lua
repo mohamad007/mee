aff32751c56ffe8d04def947b2807f1b7116a352
DRAGON <www.mohammad.blogfa111@gmail.com>
2016-07-20 00:06:51 +0430

    Add files via upload

--- plugins/lock_join.lua
+++ plugins/lock_join.lua
@@ -1,20 +1,19 @@
-local function run(msg)
-
-    local data = load_data(_config.moderation.data)
-
-     if data[tostring(msg.to.id)]['settings']['lock_join'] == 'yes' then
-
-
-if msg.to.type == 'channel' and not is_momod(msg) then
-	kick_user(msg.from.id, msg.to.id)
-
-        return 
-      end
+local function run (msg, matches)
+local data = load_data(_config.moderation.data)
+   if matches[1] == 'chat_add_user_link' then
+        local user_id = msg.from.id
+        if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+        if data[tostring(msg.to.id)]['settings']['lock_join'] == 'yes' then
+      kick_user(user_id, msg.to.id)
+    end
    end
-end
-
-return {patterns = {
-      "^!!tgservice (chat_add_user_link)$"
-}, run = run}
-
---By cyber
+end   
+ end
+ end
+return {
+  patterns = {
+    "^!!tgservice (chat_add_user_link)$"
+  },
+  run = run
+}
