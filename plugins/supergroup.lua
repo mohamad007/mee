c4b19fef295293e60b0eb44eb316dfecacd5cfb7
DRAGON <www.mohammad.blogfa111@gmail.com>
2016-07-21 19:38:31 +0430

    Add files via upload

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -2067,7 +2067,7 @@ local function run(msg, matches)
 		lock_group_bots(msg, data, target),
 		lock_group_number(msg, data, target),
 				}
-				return lock_group_links(msg, data, target),
+				return lock_group_links(msg, data, target), safemode
 			end
 			if matches[2] == 'links' then
 				savelog(msg.to.id, name_log.." ["..msg.from.id.."] locked link posting ")
@@ -2164,7 +2164,7 @@ local function run(msg, matches)
 		unlock_group_bots(msg, data, target),
 		unlock_group_number(msg, data, target),
 				}
-				return unlock_group_links(msg, data, target),
+				return unlock_group_links(msg, data, target), dsafemode
 			end
 			if matches[2] == 'links' then
 				savelog(msg.to.id, name_log.." ["..msg.from.id.."] unlocked link posting")
