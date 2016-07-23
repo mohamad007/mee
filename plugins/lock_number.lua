aba8dd7acdaeafa0c21a3dfed148ed52e8e63ab7
Cyber <www.mohammad.blogfa111@gmail.com>
2016-07-23 11:04:20 +0430

    Add files via upload

--- plugins/lock_number.lua
+++ plugins/lock_number.lua
@@ -1,45 +1,43 @@
 local function run(msg)
 
     local data = load_data(_config.moderation.data)
-	local number = msg.text:match("[0123456789٠١٢٣٤٥٦٧٨٩]")
-	local number2 = msg.media.title:match("[0123456789٠١٢٣٤٥٦٧٨٩]")
-	local number3 = msg.media.description:match("[0123456789٠١٢٣٤٥٦٧٨٩]")
-	local number4 = msg.media.caption:match("[0123456789٠١٢٣٤٥٦٧٨٩]")
-	local number5 = msg.fwd_from.title:match("[0123456789٠١٢٣٤٥٦٧٨٩]")
+	local is_number_msg = msg.text:match("[0123456789٠١٢٣٤٥٦٧٨٩]")
+	local is_number2_msg = msg.media.title:match("[0123456789٠١٢٣٤٥٦٧٨٩]")
+	local is_number3_msg = msg.media.description:match("[0123456789٠١٢٣٤٥٦٧٨٩]")
+	local is_number4_msg = msg.media.caption:match("[0123456789٠١٢٣٤٥٦٧٨٩]")
+	local is_number5_msg = msg.fwd_from.title:match("[0123456789٠١٢٣٤٥٦٧٨٩]")
 
     if data[tostring(msg.to.id)]['settings']['lock_number'] == 'yes' then
 
 
 		if msg.to.type == 'channel' and not is_momod(msg) then
 			if msg.text then
-				if number then
+				if is_number_msg then
 					delete_msg(msg.id,ok_cb,false)
 				end
 			elseif msg.media then
 				if msg.media.title then
-					if number2 then
+					if is_number2_msg then
 						delete_msg(msg.id,ok_cb,false)
 					end
-				elseif msg.media.description then
-					if number3 then
-						delete_msg(msg.id,ok_cb,false)
-					end
-				elseif msg.media.caption then
-					if number4 then
-						delete_msg(msg.id,ok_cb,false)
-					end
-				else
-					return ""
 				end
-			elseif msg.fwd_from then
-				if msg.fwd_from.title then
-					if number5 then
+				if msg.media.description then
+					if is_number3_msg then
 						delete_msg(msg.id,ok_cb,false)
 					end
+				end
+				if msg.media.caption then
+					if is_number4_msg then
+						delete_msg(msg.id,ok_cb,false)
+					end
+				end
+			elseif msg.fwd_from.title then
+				if is_number5_msg then
+					delete_msg(msg.id,ok_cb,false)
 				end
 			else
 				return ""
-			end
+		  end
 	  end
    end
 end
@@ -50,7 +48,10 @@ return {patterns = {
 	"%[(document)%]",
 	"%[(video)%]",
 	"%[(audio)%]",
-	"%[(unsupported)%]"
+	"%[(unsupported)%]",
+	"$",
+	"*",
+	"%"
 }, run = run}
 
 --By Cyber
