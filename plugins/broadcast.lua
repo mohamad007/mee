614dc50cdc6dc09c5811f63e3b26cd9f437c0a42
Cyber <www.mohammad.blogfa111@gmail.com>
2016-07-25 19:59:33 +0430

    Add files via upload

--- plugins/broadcast.lua
+++ plugins/broadcast.lua
@@ -1,11 +1,16 @@
 local function run(msg, matches)
-	if matches[1] == 'bc' and is_admin1(msg) then
-		local response = matches[3]
-		--send_large_msg("chat#id"..matches[2], response)
-		send_large_msg("channel#id"..matches[2], response)
+	if matches[1] == 'bc' then
+		if is_admin1(msg) then
+			local response = matches[3]
+			--send_large_msg("chat#id"..matches[2], response)
+			send_large_msg("channel#id"..matches[2], response)
+			return "sent"
+		else
+			return "Only sudo and admins"
+		end
 	end
 	if matches[1] == 'broadcast' then
-		if is_sudo(msg) then -- Only sudo !
+		if is_admin1(msg) then -- Only sudo and admins !
 			local data = load_data(_config.moderation.data)
 			local groups = 'groups'
 			local response = matches[2]
@@ -16,6 +21,9 @@ local function run(msg, matches)
 				send_large_msg(chat, response)
 				send_large_msg(channel, response)
 			end
+			return "sent"
+		else
+			return "Only sudo and admins"
 		end
 	end
 end
