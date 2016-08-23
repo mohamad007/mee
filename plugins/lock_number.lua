2e585bcffc401644b9f926ea0218bcd01684a608
Cyber <www.mohammad.blogfa111@gmail.com>
2016-08-23 05:42:25 -0400

    Add files via upload

--- plugins/lock_number.lua
+++ plugins/lock_number.lua
@@ -1,21 +1,47 @@
 local function run(msg)
-
-    local data = load_data(_config.moderation.data)
-
-     if data[tostring(msg.to.id)]['settings']['lock_number'] == 'yes' then
-
-
-if msg.to.type == 'channel' and not is_momod(msg) then
-	delete_msg(msg.id,ok_cb,false)
-
-        return 
-      end
-   end
+	local data = load_data(_config.moderation.data)
+	if data[tostring(msg.to.id)]['settings']['lock_number'] == 'yes' then
+		if msg.text and not is_momod(msg) then
+			if msg.text:match("[0123456789]") then
+				delete_msg(msg.id,ok_cb,false)
+			end
+		end
+		if msg.media and not is_momod(msg) then
+			if msg.media.title then
+				if msg.media.title:match("[0123456789]") then
+					delete_msg(msg.id,ok_cb,false)
+				end
+			end
+			if msg.media.description then
+				if msg.media.description:match("[0123456789]") then
+					delete_msg(msg.id,ok_cb,false)
+				end
+			end
+			if msg.media.caption then
+				if msg.media.caption:match("[0123456789]") then
+					delete_msg(msg.id,ok_cb,false)
+				end
+			end
+		end
+		if msg.fwd_from and not is_momod(msg) then
+			if msg.fwd_from.title then
+				if msg.fwd_from.title:match("[0123456789]") then
+					delete_msg(msg.id,ok_cb,false)
+				end
+			end
+		end
+	end
 end
 
-return {patterns = {
-      "[1234567890]"
-}, run = run}
+return {
+	patterns = {
+	"*",
+	"$",
+	"[0123456789]",
+	"(.*)"
+	},
+ 	run = run
+}
 
 --By Cyber
 
--- plugins/lock_username.lua
+++ plugins/lock_username.lua
@@ -1,22 +1,47 @@
 local function run(msg)
-
-    local data = load_data(_config.moderation.data)
-
-     if data[tostring(msg.to.id)]['settings']['lock_username'] == 'yes' then
-
-
-if msg.to.type == 'channel' and not is_momod(msg) then
-	delete_msg(msg.id,ok_cb,false)
-
-        return 
-      end
-   end
+	local data = load_data(_config.moderation.data)
+	if data[tostring(msg.to.id)]['settings']['lock_username'] == 'yes' then
+		if msg.text and not is_momod(msg) then
+			if msg.text:match("@") then
+				delete_msg(msg.id,ok_cb,false)
+			end
+		end
+		if msg.media and not is_momod(msg) then
+			if msg.media.title then
+				if msg.media.title:match("@") then
+					delete_msg(msg.id,ok_cb,false)
+				end
+			end
+			if msg.media.description then
+				if msg.media.description:match("@") then
+					delete_msg(msg.id,ok_cb,false)
+				end
+			end
+			if msg.media.caption then
+				if msg.media.caption:match("@") then
+					delete_msg(msg.id,ok_cb,false)
+				end
+			end
+		end
+		if msg.fwd_from and not is_momod(msg) then
+			if msg.fwd_from.title then
+				if msg.fwd_from.title:match("@") then
+					delete_msg(msg.id,ok_cb,false)
+				end
+			end
+		end
+	end
 end
 
-return {patterns = {
-      "@(.*)",
-	  "@"
-}, run = run}
+return {
+	patterns = {
+	"*",
+	"$",
+	"@",
+	"(.*)"
+	},
+ 	run = run
+}
 
 --By Cyber
 
