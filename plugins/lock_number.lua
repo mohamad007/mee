591a0774ca8aa632a7654d9dfd5e147050038d19
Cyber <www.mohammad.blogfa111@gmail.com>
2016-08-25 02:32:45 -0400

    Cyber

--- plugins/lock_number.lua
+++ plugins/lock_number.lua
@@ -1,6 +1,6 @@
 local function run(msg)
 	local data = load_data(_config.moderation.data)
-	if data[tostring(msg.to.id)]['settings']['lock_username'] == 'yes' then
+	if data[tostring(msg.to.id)]['settings']['lock_number'] == 'yes' then
 		if msg.to.type == 'channel' and not is_momod(msg) then
 			delete_msg(msg.id,ok_cb,false)
 		end
