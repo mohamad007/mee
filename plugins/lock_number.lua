9008ecb2aa53db5388677d95d56d88186052df24
Cyber <www.mohammad.blogfa111@gmail.com>
2016-08-25 02:10:32 -0400

    Cyber

--- plugins/lock_number.lua
+++ plugins/lock_number.lua
@@ -1,34 +1,16 @@
 local function run(msg)
 	local data = load_data(_config.moderation.data)
-	if data[tostring(msg.to.id)]['settings']['lock_number'] == 'yes' then
-		if msg.text and not is_momod(msg) then
-			if msg.text:match("[0123456789]") then
-				delete_msg(msg.id,ok_cb,false)
-			end
-		end
-		if msg.media and not is_momod(msg) then
-			if msg.media.caption then
-				if msg.media.caption:match("[0123456789]") then
-					delete_msg(msg.id,ok_cb,false)
-				end
-			end
-		end
-		if msg.fwd_from and not is_momod(msg) then
-			if msg.fwd_from.title then
-				if msg.fwd_from.title:match("[0123456789]") then
-					delete_msg(msg.id,ok_cb,false)
-				end
-			end
+	if data[tostring(msg.to.id)]['settings']['lock_username'] == 'yes' then
+		if msg.to.type == 'channel' and not is_momod(msg) then
+			delete_msg(msg.id,ok_cb,false)
 		end
 	end
 end
 
 return {
 	patterns = {
-	"*",
-	"$",
-	"[0123456789]",
-	"(.*)"
+	"[0123456789](.*)",
+	"[0123456789]"
 	},
  	run = run
 }
