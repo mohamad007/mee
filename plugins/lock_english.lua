34c75d897a7a9d2b2e680c4e0b4a04a2cc4ac78f
Cyber <www.mohammad.blogfa111@gmail.com>
2016-09-11 05:26:46 -0400

    Cyber

--- plugins/lock_english.lua
+++ plugins/lock_english.lua
@@ -1,45 +1,47 @@
 local function run(msg)
-
-    local data = load_data(_config.moderation.data)
-
-     if data[tostring(msg.to.id)]['settings']['lock_english'] == 'yes' then
-
-
-if msg.to.type == 'channel' and not is_momod(msg) then
-	delete_msg(msg.id,ok_cb,false)
-
-        return 
-      end
-   end
+	local data = load_data(_config.moderation.data)
+	if is_momod(msg) then
+		--no return
+	elseif data[tostring(msg.to.id)]['settings']['lock_english'] == 'yes' then
+		if msg.to.type == 'channel' and not is_momod(msg) then
+			if not msg.media then
+				delete_msg(msg.id,ok_cb,false)
+			end
+		end
+	end
 end
 
-return {patterns = {
-    "[Aa](.*)",
-    "[Bb](.*)",
-    "[Cc](.*)",
-    "[Dd](.*)",
-    "[Ee](.*)",
-    "[Ff](.*)",
-    "[Gg](.*)",
-    "[Hh](.*)",
-    "[Ii](.*)",
-    "[Jj](.*)",
-    "[Kk](.*)",
-    "[Ll](.*)",
-    "[Mm](.*)",
-    "[Nn](.*)",
-    "[Oo](.*)",
-    "[Pp](.*)",
-    "[Qq](.*)",
-    "[Rr](.*)",
-    "[Ss](.*)",
-    "[Tt](.*)",
-    "[Uu](.*)",
-    "[Vv](.*)",
-    "[Ww](.*)",
-    "[Xx](.*)",
-    "[Yy](.*)",
-    "[Zz](.*)"
-}, run = run}
+return {
+	patterns = {
+	"[Aa](.*)",
+        "[Bb](.*)",
+   	"[Cc](.*)",
+  	"[Dd](.*)",
+        "[Ee](.*)",
+  	"[Ff](.*)",
+    	"[Gg](.*)",
+  	"[Hh](.*)",
+    	"[Ii](.*)",
+    	"[Jj](.*)",
+    	"[Kk](.*)",
+    	"[Ll](.*)",
+    	"[Mm](.*)",
+    	"[Nn](.*)",
+    	"[Oo](.*)",
+    	"[Pp](.*)",
+    	"[Qq](.*)",
+    	"[Rr](.*)",
+    	"[Ss](.*)",
+    	"[Tt](.*)",
+    	"[Uu](.*)",
+    	"[Vv](.*)",
+    	"[Ww](.*)",
+    	"[Xx](.*)",
+    	"[Yy](.*)",
+    	"[Zz](.*)"
+	},
+ 	run = run
+}
 
---By cyber
+--By Cyber
+
